2e0d280021dcb206b2b81f322ac501ad1d29b3d0
Eleven-Twelv-e <nicat.maharramov.2020@gmail.com>
2021-05-08 19:01:56 +0400

    Worker

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Calculator calc = Calculator.instance.get();
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener listener1 = System.out::println;
 
-        int a = calc.plus.apply(1,2);
-        int b = calc.minus.apply(1,1);
-        int c = calc.devide.apply(4, 2);
-
-        //v pole apply mi ne peredali cifri,  v mesto cifr a i b
-
-        calc.println.accept(c);
+        Worker worker = new Worker(listener, listener1);
+        worker.start();
     }
 }
